b59173a1e87e72bac3567ed11bfcb69888e492c7
luiz <luiz@gmail.com>
2024-07-05 05:46:28 -0300

    alterando arquivos

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
 document.addEventListener('DOMContentLoaded',function(){
-    fetch('https://api.github.com/users/ogiansouza')
+    fetch('https://api.github.com/users/LuizHenriqueProfissional')
     .then(function(res){
         return res.json()
     })
     .then(function(json){
-
-        const elementAvatar = document.getElementById('avatar');
-        elementAvatar.src = json.avatar_url
-
-        const elementName = document.getElementById('name-h1');
-        elementName.innerText = json.name;
-
-        const elementUser = document.getElementById('name-h2');
-        elementUser.innerText = json.login
-
-        const seguidores = document.getElementById('seguidores');
-        seguidores.innerText = json.followers;
-
-        const seguindo = document.getElementById('seguindo');
-        seguindo.innerText = json.following;
-
-        const repositorios = document.getElementById('repositorios');
-        repositorios.innerText = json.public_repos;
-
-        const verGithub = document.getElementById('profile-link');
-        verGithub.href = json.html_url;
+        setTimeout(function(infos){
+            const elementAvatar = document.getElementById('avatar');
+            elementAvatar.src = json.avatar_url;
+    
+            const elementName = document.getElementById('name-h1');
+            elementName.innerText = json.name;
+    
+            const elementUser = document.getElementById('name-h2');
+            elementUser.innerText = json.login
+    
+            const seguidores = document.getElementById('seguidores');
+            seguidores.innerText = json.followers;
+    
+            const seguindo = document.getElementById('seguindo');
+            seguindo.innerText = json.following;
+    
+            const repositorios = document.getElementById('repositorios');
+            repositorios.innerText = json.public_repos;
+    
+            const verGithub = document.getElementById('profile-link');
+            verGithub.href = json.html_url;
+        },3000)
     })
 })
